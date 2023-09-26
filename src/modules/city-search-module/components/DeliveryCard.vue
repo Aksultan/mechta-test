053347d30eb62cf4ec10eeb68a0e91c15beca74b
Aksultan <aksultan.zhumadil@gmail.com>
2023-09-26 16:36:57 +0600

    feat: mobile layout updated

--- src/modules/city-search-module/components/DeliveryCard.vue
+++ src/modules/city-search-module/components/DeliveryCard.vue
@@ -1,14 +1,14 @@
 <template>
-        <div class="flex flex-col items-center justify-center gap-5 h-full">
-            <div v-for="delivery in delivery_types" :key="delivery.type" :class="[{'shadow': selectedDeliveryType == type}, {'pr-5': selectedDeliveryType.length == 0 || selectedDeliveryType != delivery.type}, delivery.available ? '' : 'opacity-50 pointer-events-none' , 'flex border border-color-[#E9F0EB] rounded-3xl justify-between w-[523px]']" @click="selectDeliveryType(delivery)">
-                <div class="flex flex-col justify-between gap-10 py-10 px-11 ">
-                    <h6 class="text-4xl font-bold min-w-[158px]">
+        <div class="flex flex-col items-center justify-center gap-5 h-full px-6.25">
+            <div v-for="delivery in delivery_types" :key="delivery.type" :class="[{'shadow': selectedDeliveryType == type}, {'pr-5': selectedDeliveryType.length == 0 || selectedDeliveryType != delivery.type}, delivery.available ? '' : 'opacity-50 pointer-events-none' , 'flex border border-color-[#E9F0EB] rounded-3xl justify-between min:w-[523px] w-full']" @click="selectDeliveryType(delivery)">
+                <div class="flex flex-col justify-between gap-10 py-10 sm:px-11 px-2">
+                    <h6 class="text-4xl font-bold sm:min-w-[158px]">
                         {{alias[delivery.type].label}}
                     </h6>
-                    <p class="text-5xl text-[#78A1BB] font-bold">{{delivery.price}}$</p>
+                    <p class="sm:text-5xl text-[#78A1BB] font-bold text-3xl">{{delivery.price}}$</p>
                 </div>
                 <img :src="alias[delivery.type].image" :alt="delivery.type" width="100" height="100">
-                <div v-if="selectedDeliveryType == delivery.type" class="flex items-center justify-center px-5 rounded-r-3xl  bg-gradient-to-b bg-gradient-310 from-[#FFFFFF00] to-[#65B3E4]">
+                <div v-if="selectedDeliveryType == delivery.type" class="flex items-center justify-center px-2 sm:px-5 rounded-r-3xl  bg-gradient-to-b bg-gradient-310 from-[#FFFFFF00] to-[#65B3E4]">
                     <img src="@/assets/checkmark-icon.svg" alt="checkmark">
                 </div>
             </div>
